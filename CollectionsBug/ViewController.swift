8b3bf7e0d9811bf91f93451e91a7afd6876d8cb8
vladislav <vmilenin@icloud.com>
2020-08-19 21:59:04 +0300

    Initial Commit

---
+++ CollectionsBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionsBug
+//
+//  Created by vladislav on 19.08.2020.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
